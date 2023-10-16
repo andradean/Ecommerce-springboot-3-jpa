ea1eeb903a87b5da6597bed16f28b11001b73e73
andradean <viniciusansilva@outlook.com>
2023-10-16 16:31:10 -0300

    user entity and resource

---
+++ src/main/java/com/vinicius/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.vinicius.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vinicius.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "maria", "maria@gmail", "19999", "senha");
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
