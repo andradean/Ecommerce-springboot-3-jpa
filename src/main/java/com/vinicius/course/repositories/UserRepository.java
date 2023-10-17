7d959474bc58a3b367222e283e5f0a12599c4ab6
andradean <viniciusansilva@outlook.com>
2023-10-16 23:43:28 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/vinicius/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.vinicius.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vinicius.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
